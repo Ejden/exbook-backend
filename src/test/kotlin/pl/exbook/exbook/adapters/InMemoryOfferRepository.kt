ee396f3e7bd0f85c47aa39d21a49ca514502699b
Adrian <adrian.stypinski@gmail.com>
2022-05-11 18:17:17 +0200

    fix tests

--- src/test/kotlin/pl/exbook/exbook/adapters/InMemoryOfferRepository.kt
+++ src/test/kotlin/pl/exbook/exbook/adapters/InMemoryOfferRepository.kt
@@ -1,10 +1,14 @@
 package pl.exbook.exbook.adapters
 
+import java.math.BigDecimal
 import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageImpl
 import org.springframework.data.domain.Pageable
 import pl.exbook.exbook.offer.domain.Offer
 import pl.exbook.exbook.offer.domain.OfferRepository
+import pl.exbook.exbook.shared.CategoryId
 import pl.exbook.exbook.shared.OfferId
+import pl.exbook.exbook.shared.UserId
 
 class InMemoryOfferRepository : OfferRepository {
     private val memory = mutableListOf<Offer>()
@@ -13,8 +17,12 @@ class InMemoryOfferRepository : OfferRepository {
         return memory.firstOrNull { it.id == offerId }
     }
 
+    override fun findBySellerId(sellerId: UserId, pageable: Pageable): Page<Offer> {
+        return PageImpl(memory.filter { it.seller.id == sellerId })
+    }
+
     override fun findAll(pageable: Pageable): Page<Offer> {
-        return Page.empty(pageable)
+        return PageImpl(memory)
     }
 
     override fun findAll(): List<Offer> {
@@ -24,5 +32,18 @@ class InMemoryOfferRepository : OfferRepository {
     override fun save(offer: Offer): Offer {
         memory.add(offer)
         return offer
+    }
+
+    override fun findWithFilters(
+        searchingPhrases: List<String>,
+        bookConditions: List<Offer.Condition>,
+        offerType: List<Offer.Type>,
+        priceFrom: BigDecimal?,
+        priceTo: BigDecimal?,
+        location: String?,
+        categoryId: CategoryId?,
+        pageable: Pageable
+    ): Page<Offer> {
+        return PageImpl(memory)
     }
 }
